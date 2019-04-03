9b86a97fa0d063fe81d7e6ef73a466452cc29deb
Vladimir Jovanović <vladimir.jovanovic@zamphyr.org>
2019-04-03 22:33:46 +0200

    Conditions for queries

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ const app = express();
 
 // RESTful API Endpoint
 app.get('/users', (req, res) => {
-    // Get the quiery params
+    // Get the query params
     let { name, email, limit } = req.query;
-    
+
     // Connecto to MongoDB instance
     MongoClient.connect('mongodb://localhost:27017/', (err, db) => {
         // Abort if error occures
         if (err) throw err;
         // Get the database
         let dbName = db.db('VladimirJovanovic');
-        // Search through the collection
-        dbName.collection('users').find({ email, name }, (err, res) => {
-            if (err) throw err;
-            db.close();
-        }).limit(limit);
+        
+        // If there are no query strings
+        if (!/\?.+/.test(req.url)) {
+            // Search through the collection and return all users
+            dbName.collection('users').find({}, (err, res) => {
+                if (err) throw err;
+                // Print the results to the console
+                console.log(res)
+                // Close the database connection
+                db.close();
+            });
+        } else {
+            // Search through the collection with selectors and limit
+            // the response results
+            dbName.collection('users').find({ email, name }, (err, res) => {
+                if (err) throw err;
+                // Print the results to the console
+                console.log(res)
+                // Close the database connection
+                db.close();
+            }).limit(limit);
+        }
     });
     res.end();
 });
