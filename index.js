2c47a8c6c30ae8a8a94cfa8f4a4559808dec6f39
Vladimir Jovanović <vladimir.jovanovic@zamphyr.org>
2019-04-03 21:39:56 +0200

    MongoDB setup

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Imports
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
 
 // RESTful API Endpoint
-app.get('/users', (req, res) => {
-    //
+app.get('/users/?:name?:email', (req, res) => {
+    // let { name, email } = req.query
+    let name = req.query.name;
+    let email = req.query.email;
+    // Connecto to MongoDB instance
+    MongoClient.connect('mongodb://localhost:27017/', (err, db) => {
+        // Abort if error occures
+        if (err) throw err;
+        // Get the database
+        let dbName = db.db('VladimirJovanovic');
+        // Search through the collection
+        dbName.collection('users').findOne({ email, name }, (err, res) => {
+            if (err) throw err;
+            db.close();
+        });
+    });
 });
